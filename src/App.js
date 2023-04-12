ce15ff20fd02ed854f74d9e46863dbf86278b20d
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2023-04-12 14:20:45 +0200

    first-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Header from "../src/components/header/Header";
+import ProductDetailPage from "../src/components/main/productDetailPage";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ProductDetailPage />
+    </>
+  );
+}
+
+export default App;
