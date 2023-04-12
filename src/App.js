ff756d01776323b03298a22fc85e9e7fc2d0be4d
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2023-04-12 17:25:44 +0200

    add-footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "../src/components/header/Header";
 import ProductDetailPage from "../src/components/main/productDetailPage";
+import Footer from "../src/components/footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <ProductDetailPage />
+      <Footer />
     </>
   );
 }
