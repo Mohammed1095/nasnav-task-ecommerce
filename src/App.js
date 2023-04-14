9909422119b6021f469ab25699e89248d76b6b54
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2023-04-14 23:04:15 +0200

    update product card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "../src/components/header/Header";
 import Main from "../src/components/main/main";
 import Footer from "../src/components/footer/Footer";
+import { adidas } from "../src/assets/icons/adidas-logo";
 
 function App() {
   const products = [
@@ -9,47 +10,43 @@ function App() {
       id: 1,
       title: "Product 1",
       price: "$19.99",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
       image: require("../src/assets/icons/shirtCart.png"),
+      brandLogo: { adidas },
     },
     {
       id: 2,
       title: "Product 2",
       price: "$24.99",
-      description:
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
       image: require("../src/assets/icons/shirtCart.png"),
     },
     {
       id: 3,
       title: "Product 3",
       price: "$29.99",
-      description:
-        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
       image: require("../src/assets/icons/shirtCart.png"),
     },
     {
       id: 4,
       title: "Product 4",
       price: "$29.99",
-      description:
-        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
       image: require("../src/assets/icons/shirtCart.png"),
     },
     {
       id: 5,
       title: "Product 5",
       price: "$29.99",
-      description:
-        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
       image: require("../src/assets/icons/shirtCart.png"),
     },
     {
       id: 6,
       title: "Product 5",
       price: "$29.99",
-      description:
-        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
       image: require("../src/assets/icons/shirtCart.png"),
     },
     {
--- src/components/main/components/productCard/ProductCard.js
+++ src/components/main/components/productCard/ProductCard.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./ProductCard.scss";
+import { adidas } from "../../../../assets/icons/adidas-logo";
+import RatingDisplay from "../ratingDisplay/RatingDisplay";
 
 const ProductCard = ({ product }) => {
+  const { image, name, description, price, rating, brandLogo } = product;
+
   return (
     <div className="product-card">
-      <div className="product-image-placeholder">
-        <img src={product.image} alt="Logo" />
-      </div>
-      <div className="product-info">
-        <h3 className="product-title">{product.title}</h3>
-        <p className="product-price">{product.price}</p>
+      <img className="product-card__image" src={image} alt={name} />
+      <div className="product-card__info">
+        <div className="product-card__info__description">{description}</div>
+        <div className="product-card__info__price-logo">
+          <div className="product-card__info__price"> {price}</div>
+          <div className="product-card__info__brand-logo">{adidas}</div>
+        </div>
+        <div className="product-card__info__rating">
+          <RatingDisplay />
+          <p>4.2 of 5</p>
+          <p>22 rates</p>
+        </div>
+        <p></p>
       </div>
     </div>
   );
