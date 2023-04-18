50ea39127ebb6be6e70b0b52c859285f802e33e0
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2023-04-18 16:45:00 +0200

    Update-Detail-page

---
+++ src/assets/icons/main-icons.js
@@ -0,0 +1,33 @@
+const arrowLeftIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='15.589'
+    height='24'
+    viewBox='0 0 15.589 24'
+    className='arrows-icons'
+  >
+    <path
+      className='a'
+      d='M7373.221,1274.066a.785.785,0,0,0,.491-1.4l-13.252-10.6,13.252-10.6a.785.785,0,1,0-.981-1.226l-14.018,11.215a.784.784,0,0,0,0,1.226l14.018,11.215A.781.781,0,0,0,7373.221,1274.066Z'
+      transform='translate(-7358.418 -1250.066)'
+    />
+  </svg>
+);
+
+const arrowRightIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='15.589'
+    height='24'
+    viewBox='0 0 15.589 24'
+    className='arrows-icons'
+  >
+    <path
+      className='a'
+      d='M7911.615,1250.066a.785.785,0,0,0-.491,1.4l13.252,10.6-13.252,10.6a.785.785,0,1,0,.981,1.226l14.018-11.215a.784.784,0,0,0,0-1.226l-14.018-11.215A.787.787,0,0,0,7911.615,1250.066Z'
+      transform='translate(-7910.829 -1250.066)'
+    />
+  </svg>
+);
+
+export { arrowLeftIcon, arrowRightIcon };
